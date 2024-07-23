74bf1969e42538e6fef701c0fa06a2228fc37a10
agnihotrivarsha <39787917+agnihotrivarsha@users.noreply.github.com>
2024-07-23 20:27:54 +0530

    Update Login.js

--- MoneyTracker/frontend/src/view/Login.js
+++ MoneyTracker/frontend/src/view/Login.js
@@ -1,21 +1,21 @@
 import { Link } from 'react-router-dom'
 const Login = () => {
     return (
-        <form id="loginForm" class="loginForm">
+        <form id="loginForm" class="loginForm" role="form">
             <div>
                 <div>
-                    <label for="username">Username:</label>
-                    <input type="email" id="username" name="username" required placeholder="a@b.com" />
+                    <label id="username-label" for="username">Username:</label>
+                    <input type="email" id="username" name="username" required placeholder="a@b.com" aria-labelledby="username-label" />
                 </div>
 
                 <div>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" required placeholder="*****" />
+                    <label id="password-label" for="password">Password:</label>
+                    <input type="password" id="password" name="password" required placeholder="*****" aria-labelledby="password-label" />
                 </div>
             </div>
 
             <div>
-                <Link class="loginBtn" to="/dashboard">Login</Link>
+                <Link to="/dashboard" class="loginBtn" role="button">Login</Link>
             </div>
         </form>
     );
