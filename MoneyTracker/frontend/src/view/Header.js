56714cfd92c0a8bd931c0ba02a54a1e57746389a
agnihotrivarsha <39787917+agnihotrivarsha@users.noreply.github.com>
2024-07-23 20:18:17 +0530

    Update Header.js

--- MoneyTracker/frontend/src/view/Header.js
+++ MoneyTracker/frontend/src/view/Header.js
@@ -10,12 +10,14 @@ const Header = () => {
               Money Tracker
             </a>
           </div>
-          <ul className="nav-links">
-            <li><a href="/">Dashboard</a></li>            
-            <li><a href="/profile">Profile</a></li>
-            <li><a href="/contact-us">Contact us</a></li>
-            <li><a href="/about-us">About Us</a></li>
-          </ul>
+          <div role="navigation">
+              <ul className="nav-links">
+                <li><a href="/">Dashboard</a></li>            
+                <li><a href="/profile">Profile</a></li>
+                <li><a href="/contact-us">Contact us</a></li>
+                <li><a href="/about-us">About Us</a></li>
+              </ul>
+            </div>
         </nav>
       </header>
     );
